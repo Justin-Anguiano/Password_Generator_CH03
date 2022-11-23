d79a7ce1d9ee2997e114aef44e6a91fa61c98c39
Justin_Anguiano <justin.anguiano@cox.net>
2022-11-23 10:46:23 -0600

    refactored JS

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
 // Assignment Code
-function askButton() {
-  let pwlength = prompt("Hello Human! How many characters would you like in your password, 8-71?");
-  if (pwlength <= 7) 
-  return alert("Try again please!  Password must be 8-71 characters long!")
-  if (pwlength >= 72) {
-  return alert("Try again please!  Password must be 8-71 characters long!")
-  
+var charNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function getPasswordCriteria() {
+  var userChoseNumbers = confirm("Do you want numbers in your password?");
+  var options = {
+    userChoseNumbers: userChoseNumbers
+  };
+  return options;
 }
+
+function createRandom(length) {
+  var randomNum = Math.floor(Math.random() * length);
+  return randomNum;
+}
+
+function generatePassword() {
+    var userChose = getPasswordCriteria();
+    var availableChars = [];
+    var passwordArr = [];
+    if(userChose.userChoseNumbers) {
+      availableChars = availableChars.concat(numbers);
+    }
+
+    for(var i = 0; i < 4; i++) {
+      passwordArr.push(availableChars[createRandom(availableChars.length)])
+    }
+
+
+  return passwordArr.join("")
+}
+
+/* Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+ /*let charUpperCs = prompt("Would you like to use upper case letters?");
+  if (charUpperCs === true)
+
+  
+  
+  else prompt = ("Try again! You must select between 1-10.")
+  if (charNumSpecial >= 11)
+  return alert = ("Try again! You must select between 1-10.")
+  
+
  //if (pwlength === )*/
 
   /*confirm("Would you like to use Lower Case letters?");
   if ()*/
-}
+
 
 
 
@@ -21,9 +68,10 @@ function askButton() {
 //Write password to the #password input
 /*function writePassword() {}
   var password = generatePassword();
-  var charNumSpecial = ["1","2","3","4",5,6,7,8,9,0,!,@,#,$,%,^,&,*,(,));
+  var charNum = ["0","1","2","3","4","5","6","7","8","9"];
   var charLowerCs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var charUpperCs = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+  var charSpec =   ["!","@","#","$","%","^","&","*","(",")"]
   var passwordText = document.querySelector("#password");
 
   for (var x = 0; x <=pwlength.length; x++) {}
