77ec7662fe9c97c83ffca7ba4f026afdc54a8ba9
Justin_Anguiano <justin.anguiano@cox.net>
2022-11-27 15:32:41 -0600

    JS updates to Spec Char, Numbers, Lower Cs and Upper Cs

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Assignment Code
-var charNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Arrays Utilized
+var charNumbers = ['0','1','2','3','4','5','6','7','8','9'];
+var charLowerCs = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var charUpperCs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var charSpec =   ['!','@','#','$','%','^','&','*','(',')']
 
 function getPasswordCriteria() {
-  var userChoseNumbers = confirm("Do you want numbers in your password?");
+  var userPickNumbers = confirm('Hello Human! Do you want numbers in your password?');
+  var userPickLowerCs = confirm('Do you want lower case letters in your password?');
+  var userPickUpperCs = confirm('Do you want upper case letters in your password?');
+  var userPickCharSpec = confirm('Do you want special characters in your password?');
   var options = {
-    userChoseNumbers: userChoseNumbers
+    userPickNumbers: userPickNumbers,
+    userPickLowerCs: userPickLowerCs,
+    userPickUpperCs: userPickUpperCs,
+    userPickCharSpec: userPickCharSpec
   };
   return options;
 }
@@ -15,10 +24,10 @@ function createRandom(length) {
 }
 
 function generatePassword() {
-    var userChose = getPasswordCriteria();
+    var userPick = getPasswordCriteria();
     var availableChars = [];
     var passwordArr = [];
-    if(userChose.userChoseNumbers) {
+    if(userPick.userPickNumbers) {
       availableChars = availableChars.concat(numbers);
     }
 
@@ -27,57 +36,20 @@ function generatePassword() {
     }
 
 
-  return passwordArr.join("")
+  return passwordArr.join('')
 }
 
-/* Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- /*let charUpperCs = prompt("Would you like to use upper case letters?");
-  if (charUpperCs === true)
-
-  
-  
-  else prompt = ("Try again! You must select between 1-10.")
-  if (charNumSpecial >= 11)
-  return alert = ("Try again! You must select between 1-10.")
-  
-
- //if (pwlength === )*/
-
-  /*confirm("Would you like to use Lower Case letters?");
-  if ()*/
-
-
-
-
-
-// var generateBtn = document.querySelector("#generate");
-
-//Write password to the #password input
-/*function writePassword() {}
-  var password = generatePassword();
-  var charNum = ["0","1","2","3","4","5","6","7","8","9"];
-  var charLowerCs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var charUpperCs = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-  var charSpec =   ["!","@","#","$","%","^","&","*","(",")"]
-  var passwordText = document.querySelector("#password");
-
-  for (var x = 0; x <=pwlength.length; x++) {}
-  passwordText.value = password;*/
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
